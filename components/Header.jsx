655f43243b53aa93d765af2b5619580f1f55b2d2
Stephen Nguyen <111164465+tungbinhdinh89@users.noreply.github.com>
2023-05-20 20:48:29 +0700

    done with frontend

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,25 @@
-import React from 'react';
+import Link from 'next/link';
+import React, { useState, useEffect } from 'react';
+import Menu from './Menu';
+import Wrapper from './Wrapper';
 
 const Header = () => {
-  return <div>Header</div>;
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState('translate-y-0');
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  return (
+    <div
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300`}>
+      <Wrapper>
+        <Link href="/">
+          <img src="/logo.svg" className="w-[40px] md:w-[60px]" />
+        </Link>
+        <Menu />
+      </Wrapper>
+    </div>
+  );
 };
 
 export default Header;
